077783752cd70f4594186ccd61bb1a8da77b9b7a
jess <callmejessxx2@gmail.com>
2021-01-01 19:22:58 +0900

    commit please

---
+++ src/again/Bobo.java
@@ -0,0 +1,13 @@
+package again;
+
+public class Bobo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String value = "1월1일";
+
+		System.out.println(value);
+	}
+
+}
